c848e5a8ce9bd34eaa9686eb97ece8562ef5c3a1
Jason King <jk1ng@yahoo.com>
2023-05-11 14:21:49 -0700

    Fixed error in l3switch day0 config

--- plugins/modules/cml_lab.py
+++ plugins/modules/cml_lab.py
@@ -337,7 +337,76 @@ interface GigabitEthernet0/0
  ip address dhcp
  no shutdown
 !
-interface GigabitEthernet1/0/1-24
+interface GigabitEthernet1/0/1
+ no switchport
+!
+interface GigabitEthernet1/0/2
+ no switchport
+!
+interface GigabitEthernet1/0/3
+ no switchport
+!
+interface GigabitEthernet1/0/4
+ no switchport
+!
+interface GigabitEthernet1/0/5
+ no switchport
+!
+interface GigabitEthernet1/0/6
+ no switchport
+!
+interface GigabitEthernet1/0/7
+ no switchport
+!
+interface GigabitEthernet1/0/8
+ no switchport
+!
+interface GigabitEthernet1/0/9
+ no switchport
+!
+interface GigabitEthernet1/0/10
+ no switchport
+!
+interface GigabitEthernet1/0/11
+ no switchport
+!
+interface GigabitEthernet1/0/12
+ no switchport
+!
+interface GigabitEthernet1/0/13
+ no switchport
+!
+interface GigabitEthernet1/0/14
+ no switchport
+!
+interface GigabitEthernet1/0/15
+ no switchport
+!
+interface GigabitEthernet1/0/16
+ no switchport
+!
+interface GigabitEthernet1/0/17
+ no switchport
+!
+interface GigabitEthernet1/0/18
+ no switchport
+!
+interface GigabitEthernet1/0/19
+ no switchport
+!
+interface GigabitEthernet1/0/20
+ no switchport
+!
+interface GigabitEthernet1/0/21
+ no switchport
+!
+interface GigabitEthernet1/0/22
+ no switchport
+!
+interface GigabitEthernet1/0/23
+ no switchport
+!
+interface GigabitEthernet1/0/24
  no switchport
 !
 no ip http server
