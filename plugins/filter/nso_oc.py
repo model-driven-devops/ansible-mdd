0a281fb2a285eed339ffcaf1c790877fab26584e
Jason King <jk1ng@yahoo.com>
2025-10-24 12:30:42 -0700

    Updates for ansible 2.19 and python3.13

--- plugins/filter/nso_oc.py
+++ plugins/filter/nso_oc.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-from ansible.module_utils.six import raise_from
 from ansible.errors import AnsibleError
 import copy
 
@@ -16,7 +15,7 @@ else:
 def nso_oc(config_data):
 
     if NSO_OC_IMPORT_ERROR:
-        raise_from(AnsibleError('nso-oc must be installed to use this plugin'), NSO_OC_IMPORT_ERROR)
+        raise AnsibleError('nso-oc must be installed to use this plugin') from NSO_OC_IMPORT_ERROR
 
     mdd_dict = {
         "mdd_data": {}
--- plugins/lookup/netbox_oc.py
+++ plugins/lookup/netbox_oc.py
@@ -105,7 +105,6 @@ from re import search, findall, IGNORECASE
 from ansible.errors import AnsibleError
 from ansible.plugins.lookup import LookupBase
 from ansible.utils.display import Display
-from ansible.module_utils.six import raise_from
 
 try:
     import pynetbox
@@ -560,16 +559,10 @@ class LookupModule(LookupBase):
 
     def run(self, terms, variables=None, **kwargs):
         if PYNETBOX_LIBRARY_IMPORT_ERROR:
-            raise_from(
-                AnsibleError("pynetbox must be installed to use this plugin"),
-                PYNETBOX_LIBRARY_IMPORT_ERROR,
-            )
+            raise AnsibleError("pynetbox must be installed to use this plugin") from PYNETBOX_LIBRARY_IMPORT_ERROR
 
         if REQUESTS_LIBRARY_IMPORT_ERROR:
-            raise_from(
-                AnsibleError("requests must be installed to use this plugin"),
-                REQUESTS_LIBRARY_IMPORT_ERROR,
-            )
+            raise AnsibleError("requests must be installed to use this plugin") from REQUESTS_LIBRARY_IMPORT_ERROR
 
         netbox_api_token = (
             kwargs.get("token")
